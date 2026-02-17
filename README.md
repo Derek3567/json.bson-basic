a213a71a4a99af2a8cfefb70cb00abf749030c06
Derek3567 <derek.vallejo.alp@cbtis258.edu.mx>
2026-02-17 11:40:22 -0600

    Add files via upload

---
+++ README.md
@@ -0,0 +1,7 @@
+Ejemplos sencillos de objetos en JS 
+
+
+Ejemplos sencillos de JSON con difeentes tipos de datos 
+
+
+Ejemplos sencillos de estructura BSON
---
+++ datatypes.js
@@ -0,0 +1,38 @@
+let usern = "Sergio";
+let age = "35";
+let isActive = true;
+let minecraft = [ "reading", "programing", "jugar terraria", "programar gta VIII"];
+let fps = 1
+let gta_VI = true
+
+// console.log(usern)
+// console.log(typeof(usern))
+
+// console.log(age)
+// console.log(typeof(age))
+
+// console.log(isActive)
+// console.log(typeof(isActive))
+
+// console.log(gta_VI)
+// console.log(typeof(gta_VI))
+
+// console.log(minecraft[0])
+
+
+
+let Configuracion_grafica = {
+    "calidad": "Ultra",
+    "resolucion": "QHD",
+    "ray_trazing": true,
+    "renderizado_en_chunks": 64,
+    "shaders": {
+        "bajos": "vanilla",
+        "medios": "photon",
+        "altos": "ZEUS"
+    },
+    "max_fotogramas": 120,
+    "antialising": true
+};
+
+console.table(Configuracion_grafica)
